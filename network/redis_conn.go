17a39a09e42c6463ff6d5fea35e54a9bd3cf1bbc
wenbinmeng <wenbinmeng@creditease.cn>
2016-08-10 20:25:31 +0800

    Updated Code Struct

--- network/redis_conn.go
+++ network/redis_conn.go
@@ -3,20 +3,8 @@ package network
 import (
 	"bufio"
 	"errors"
-	"github.com/mijia/sweb/log"
 	"net"
-	"strconv"
-	"time"
-)
-
-const (
-	SIMPLE_STRING = '+'
-	BULK_STRING   = '$'
-	INTEGER       = ':'
-	ARRAY         = '*'
-	ERROR         = '-'
-
-	SYM_CRLF = "\r\n"
+	// "time"
 )
 
 var (
@@ -26,7 +14,7 @@ var (
 
 type RedisConn struct {
 	*Conn
-	br *bufio.Reader
+	*RedisReader
 }
 
 func NewRedisConn(conn net.Conn, co *ConnectOption) (*RedisConn, error) {
@@ -35,81 +23,11 @@ func NewRedisConn(conn net.Conn, co *ConnectOption) (*RedisConn, error) {
 		return nil, err
 	}
 	r := &RedisConn{Conn: cn}
-	r.br = bufio.NewReader(r)
+	r.RedisReader = NewRedisReader(bufio.NewReader(r))
 	return r, nil
 }
 
 func (r *RedisConn) ReadAll() ([]byte, error) {
-	res := make([]byte, 0)
-	err := r.readObject(&res)
-	return res, err
-}
-
-func (r *RedisConn) readObject(res *[]byte) error {
-	r.conn.SetReadDeadline(time.Now().Add(r.cnop.readTimeOutSec))
-	line, err := r.readLine()
-	if err != nil {
-		log.Error(err.Error())
-		return err
-	}
-	*res = append(*res, []byte(line)...)
-	switch line[0] {
-	case SIMPLE_STRING, INTEGER, ERROR:
-		return nil
-	case BULK_STRING:
-		return r.readBulkString(line, res)
-	case ARRAY:
-		return r.readArray(line, res)
-	default:
-		return BAD_ELEMENT_ERR
-	}
-}
-
-func (r *RedisConn) readLine() ([]byte, error) {
-	p, err := r.br.ReadSlice('\n')
-	if err != nil {
-		return nil, err
-	}
-	return p, nil
-}
-
-func (r *RedisConn) getCount(line []byte) (int, error) {
-	if !(line[len(line)-1] == '\n' && line[len(line)-2] == '\r') {
-		return 0, BAD_ELEMENT_ERR
-	}
-	return strconv.Atoi(string(line[1 : len(line)-2]))
-}
-
-func (r *RedisConn) readBulkString(line []byte, res *[]byte) error {
-	size, err := r.getCount(line)
-	if err != nil {
-		return err
-	}
-	if size == -1 {
-		return nil
-	}
-	if size < 0 {
-		return BAD_ELEMENT_ERR
-	}
-	size = size + 2
-	buffer, err := r.br.Peek(size)
-	r.br.Discard(size)
-	*res = append(*res, buffer...)
-	return err
-}
-
-func (r *RedisConn) readArray(line []byte, res *[]byte) error {
-	// Get number of array elements.
-	count, err := r.getCount(line)
-	if err != nil {
-		return err
-	}
-	// Read `count` number of RESP objects in the array.
-	for i := 0; i < count; i++ {
-		err := r.readObject(res)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	// r.conn.SetReadDeadline(time.Now().Add(r.cnop.readTimeOutSec))
+	return r.ReadObject()
 }
--- network/util.go
+++ network/util.go
@@ -1,23 +1,35 @@
 package network
 
 import (
-	"github.com/mijia/sweb/log"
+	"bufio"
+	"io"
+	"os"
+	"strconv"
 	"syscall"
 	"time"
 )
 
-func SyscallWrite(fd int, b []byte, aeBufferSize int) error {
+const (
+	SIMPLE_STRING = '+'
+	BULK_STRING   = '$'
+	INTEGER       = ':'
+	ARRAY         = '*'
+	ERROR         = '-'
+
+	SYM_CRLF = "\r\n"
+)
+
+func SyscallWrite(fd int, b *[]byte, aeBufferSize int) error {
 	from := 0
 	to := 0
-	respSize := len(b)
+	respSize := len(*b)
 	for {
 		if from+aeBufferSize < respSize {
 			to = from + aeBufferSize
 		} else {
 			to = respSize
 		}
-		if n, err := syscall.Write(fd, b[from:to]); err != nil {
-			log.Error(err.Error())
+		if n, err := syscall.Write(fd, (*b)[from:to]); err != nil {
 			if err == syscall.EAGAIN || err == syscall.EWOULDBLOCK {
 				time.Sleep(10 * time.Millisecond)
 				continue
@@ -33,18 +45,131 @@ func SyscallWrite(fd int, b []byte, aeBufferSize int) error {
 	return nil
 }
 
-func SyscallRead(fd int, aeBufferSize int) ([]byte, error) {
-	buf := make([]byte, aeBufferSize)
-	msg := make([]byte, 0)
+type FdReader struct {
+	fd int
+}
+
+func NewFdReader(fd int) *FdReader {
+	return &FdReader{fd: fd}
+}
+
+func (f *FdReader) Read(b []byte) (n int, err error) {
 	for {
-		nbytes, err := syscall.Read(fd, buf)
-		msg = append(msg, buf[:nbytes]...)
+		n, err = syscall.Read(f.fd, b)
 		if err != nil {
-			return nil, err
-		} else if nbytes < aeBufferSize {
+			n = 0
+			if err == syscall.EAGAIN {
+				return n, err
+			}
+		}
+		break
+	}
+	if _, ok := err.(syscall.Errno); ok {
+		err = os.NewSyscallError("read", err)
+	}
+	return
+}
+
+func SyscallRead(fd int, aeBufferSize int) ([]byte, error) {
+	fr := NewFdReader(fd)
+	br := bufio.NewReader(fr)
+	redisReader := NewRedisReader(br)
+	msg := make([]byte, 0)
+	var err error
+	for {
+		if bf, e := redisReader.ReadObject(); e == nil {
+			msg = append(msg, bf...)
+		} else {
+			if pe, ok := e.(*os.PathError); ok {
+				e = pe.Err
+			}
+			if e == syscall.EAGAIN || e == syscall.EWOULDBLOCK || e == io.EOF {
+				break
+			}
+			err = e
 			break
 		}
-
 	}
-	return msg, nil
+	return msg, err
+}
+
+type RedisReader struct {
+	br *bufio.Reader
+}
+
+func NewRedisReader(br *bufio.Reader) *RedisReader {
+	return &RedisReader{br: br}
+}
+
+func (r *RedisReader) ReadObject() ([]byte, error) {
+	res := make([]byte, 0)
+	err := r.readObject(&res)
+	return res, err
+}
+
+func (r *RedisReader) readObject(res *[]byte) error {
+	line, err := r.readLine()
+	if err != nil {
+		return err
+	}
+	*res = append(*res, []byte(line)...)
+	switch line[0] {
+	case SIMPLE_STRING, INTEGER, ERROR:
+		return nil
+	case BULK_STRING:
+		return r.readBulkString(&line, res)
+	case ARRAY:
+		return r.readArray(&line, res)
+	default:
+		return BAD_ELEMENT_ERR
+	}
+}
+
+func (r *RedisReader) readLine() ([]byte, error) {
+	p, err := r.br.ReadSlice('\n')
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (r *RedisReader) getCount(line *[]byte) (int, error) {
+	if !((*line)[len(*line)-1] == '\n' && (*line)[len(*line)-2] == '\r') {
+		return 0, BAD_ELEMENT_ERR
+	}
+	return strconv.Atoi(string((*line)[1 : len(*line)-2]))
+}
+
+func (r *RedisReader) readBulkString(line *[]byte, res *[]byte) error {
+	size, err := r.getCount(line)
+	if err != nil {
+		return err
+	}
+	if size == -1 {
+		return nil
+	}
+	if size < 0 {
+		return BAD_ELEMENT_ERR
+	}
+	size = size + 2
+	buffer, err := r.br.Peek(size)
+	r.br.Discard(size)
+	*res = append(*res, buffer...)
+	return err
+}
+
+func (r *RedisReader) readArray(line *[]byte, res *[]byte) error {
+	// Get number of array elements.
+	count, err := r.getCount(line)
+	if err != nil {
+		return err
+	}
+	// Read `count` number of RESP objects in the array.
+	for i := 0; i < count; i++ {
+		err := r.readObject(res)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
